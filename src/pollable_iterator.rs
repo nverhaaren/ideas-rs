14881edb21e0b4a667a70f8f91eb7e7fe6cbd1b8
Nathaniel Verhaaren <nverhaaren94@gmail.com>
2023-04-20 09:05:46 -0500

    code complete pollable_queue with transform_expose

--- src/pollable_iterator.rs
+++ src/pollable_iterator.rs
@@ -1,3 +1,10 @@
+use std::cell::{Cell, RefCell};
+use std::collections::VecDeque;
+use std::iter::{Fuse, FusedIterator};
+use std::marker::PhantomData;
+
+pub struct Hidden(());
+
 pub trait PollableIterator: Iterator {
     fn is_done(&self) -> bool;
 
@@ -5,15 +12,38 @@ pub trait PollableIterator: Iterator {
     where F: FnMut(Option<Self::Item>) -> Option<B>, Self: Sized {
         Transform { it: self, f, done: false }
     }
+
+    fn transform_expose<B, F, X>(self, f: F, x: X) -> TransformExpose<Self, F, X>
+    where F: FnMut(Option<Self::Item>) -> Option<B>, Self: Sized {
+        TransformExpose{ transform: Transform { it: self, f, done: false }, x }
+    }
+
+    fn from_fused<I: FusedIterator>(it: I) -> FromFused<I> {
+        FromFused { it, done: false }
+    }
 }
 
+#[derive(Debug)]
 pub struct Transform<I, F> {
     it: I,
     f: F,
     done: bool,
 }
 
-impl<I: PollableIterator, B, F: FnMut(Option<I::Item>) -> Option<B>> Iterator for Transform<I, F> {
+#[derive(Debug)]
+pub struct TransformExpose<I, F, X> {
+    transform: Transform<I, F>,
+    x: X,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct FromFused<I> {
+    it: I,
+    done: bool,
+}
+
+impl<I, B, F> Iterator for Transform<I, F>
+where I: PollableIterator, F: FnMut(Option<I::Item>) -> Option<B> {
     type Item = B;
     fn next(&mut self) -> Option<Self::Item> {
         if self.done {
@@ -31,7 +61,49 @@ impl<I: PollableIterator, B, F: FnMut(Option<I::Item>) -> Option<B>> Iterator fo
     }
 }
 
-impl<I: PollableIterator, B, F: FnMut(Option<I::Item>) -> Option<B>> PollableIterator for Transform<I, F> {
+impl<I, F, X, W> TransformExpose<I, F, X>
+where X: Fn(&mut I) -> W {
+    pub fn expose(&mut self) -> W {
+        (self.x)(&mut self.transform.it)
+    }
+}
+
+impl<I, B, F> PollableIterator for Transform<I, F>
+where I: PollableIterator, F: FnMut(Option<I::Item>) -> Option<B> {
+    fn is_done(&self) -> bool {
+        self.done
+    }
+}
+
+impl<I, B, F, X> Iterator for TransformExpose<I, F, X>
+where I: PollableIterator, F: FnMut(Option<I::Item>) -> Option<B> {
+    type Item = B;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.transform.next()
+    }
+}
+
+impl<I, B, F, X> PollableIterator for TransformExpose<I, F, X>
+where I: PollableIterator, F: FnMut(Option<I::Item>) -> Option<B> {
+    fn is_done(&self) -> bool {
+        self.transform.is_done()
+    }
+}
+
+impl<I: FusedIterator> Iterator for FromFused<I> {
+    type Item = I::Item;
+    fn next(&mut self) -> Option<Self::Item> {
+        let result = self.it.next();
+        if result.is_none() {
+            self.done = true;
+        }
+        result
+    }
+}
+
+impl<I: FusedIterator> FusedIterator for FromFused<I> {}
+
+impl<I: FusedIterator> PollableIterator for FromFused<I> {
     fn is_done(&self) -> bool {
         self.done
     }
---
+++ src/pollable_queue.rs
@@ -0,0 +1,157 @@
+use std::collections::VecDeque;
+use crate::PollableIterator;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct PollableQueue<T> {
+    queue: VecDeque<T>,
+    closed: bool,
+}
+
+#[derive(Debug)]
+pub struct PollableQueueBack<'a, T> {
+    inner: &'a mut PollableQueue<T>,
+}
+
+impl<T> PollableQueue<T> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn push(&mut self, t: T) {
+        assert!(!self.closed);
+        self.queue.push_back(t);
+    }
+
+    pub fn close(&mut self) {
+        self.closed = true;
+    }
+
+    pub fn closed(&self) -> bool {
+        self.closed
+    }
+
+    pub fn expose(&mut self) -> PollableQueueBack<T> {
+        PollableQueueBack { inner: self }
+    }
+}
+
+impl<'a, T> PollableQueueBack<'a, T> {
+    pub fn push(&mut self, t: T) {
+        self.inner.push(t);
+    }
+
+    pub fn close(&mut self) {
+        self.inner.close();
+    }
+
+    pub fn closed(&self) -> bool {
+        self.inner.closed()
+    }
+}
+
+impl<T> Default for PollableQueue<T> {
+    fn default() -> Self {
+        Self { queue: Default::default(), closed: Default::default() }
+    }
+}
+
+impl<T> Extend<T> for PollableQueue<T> {
+    fn extend<I: IntoIterator<Item=T>>(&mut self, iter: I) {
+        assert!(!self.closed);
+        self.queue.extend(iter)
+    }
+}
+
+impl<T> FromIterator<T> for PollableQueue<T> {
+    fn from_iter<I: IntoIterator<Item=T>>(iter: I) -> Self {
+        Self { queue: iter.into_iter().collect(), closed: false }
+    }
+}
+
+impl<T> Iterator for PollableQueue<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.queue.pop_front()
+    }
+}
+
+impl<T> PollableIterator for PollableQueue<T> {
+    fn is_done(&self) -> bool {
+        self.closed() && self.queue.is_empty()
+    }
+}
+
+impl<'a, T> From<&'a mut PollableQueue<T>> for PollableQueueBack<'a, T> {
+    fn from(value: &'a mut PollableQueue<T>) -> Self {
+        Self { inner: value }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::collections::VecDeque;
+    use std::mem;
+    use crate::pollable_iterator::{Transform, TransformExpose};
+    use crate::pollable_queue::{PollableQueue, PollableQueueBack};
+    use crate::PollableIterator;
+
+    struct Extractor {
+        contents: VecDeque<String>,
+        working: String,
+        receiving: bool,
+    }
+
+    impl Extractor {
+        fn new() -> Self {
+            Self { contents: Default::default(), working: Default::default(), receiving: true }
+        }
+
+        fn accept(&mut self) {
+            self.contents.push_back(mem::take(&mut self.working));
+        }
+
+        fn reject(&mut self) {
+            self.working.truncate(0);
+            self.receiving = false;
+        }
+
+        fn process(&mut self, c: char) {
+            match (self.receiving, c) {
+                (false, ' ') => self.receiving = true,
+                (true, ' ') => if !self.working.is_empty() { self.accept() },
+                (true, _) => if c.is_ascii_uppercase() {
+                    self.working.push(c);
+                } else {
+                    self.reject();
+                }
+                _ => (),
+            }
+        }
+
+        fn process_many(&mut self, cs: impl IntoIterator<Item=char>) {
+            cs.into_iter().for_each(|c| self.process(c))
+        }
+
+        fn pop(&mut self) -> Option<String> {
+            self.contents.pop_front()
+        }
+    }
+
+    fn make_upper_extractor() -> TransformExpose<
+        PollableQueue<String>,
+        impl FnMut(Option<String>) -> Option<String>,
+        impl Fn(&mut PollableQueue<String>) -> PollableQueueBack<String>
+    > {
+        let queue: PollableQueue<String> = PollableQueue::new();
+        let mut extractor = Extractor::new();
+        queue.transform_expose(
+            move |maybe_s| {
+                if let Some(s) = maybe_s {
+                    extractor.process_many(s.chars());
+                }
+                extractor.contents.pop_front()
+            },
+            PollableQueue::expose
+        )
+    }
+}
